284476c88e33b0d4f2ad256d772a4f53c4c249f3
niezheng1 <niezheng1@jd.com>
2019-07-15 10:10:54 +0800

    server

---
+++ src/main/java/com/zheng/nie/springboot/urule/common/URuleServletRegistration.java
@@ -0,0 +1,25 @@
+package com.zheng.nie.springboot.urule.common;
+
+import com.bstek.urule.console.servlet.URuleServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/15 10:08
+ */
+@Component
+public class URuleServletRegistration {
+
+
+    @Bean
+    public ServletRegistrationBean<HttpServlet> registerURuleServlet()
+    {
+        return new ServletRegistrationBean(new URuleServlet(), new String[] { "/urule/*" });
+    }
+
+
+}
---
+++ src/main/java/com/zheng/nie/springboot/urule/config/datasource/Configuration.java
@@ -0,0 +1,32 @@
+package com.zheng.nie.springboot.urule.config.datasource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import javax.sql.DataSource;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/15 10:05
+ */
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourceLoader() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setIgnoreUnresolvablePlaceholders(true);
+        configurer.setOrder(1);
+        return configurer;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource datasource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
