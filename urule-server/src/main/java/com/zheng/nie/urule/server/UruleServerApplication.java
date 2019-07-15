b176deb36e80efe3056ba027fcc57d8427f53fb4
niezheng1 <niezheng1@jd.com>
2019-07-15 10:15:13 +0800

    add

---
+++ urule-server/src/main/java/com/zheng/nie/urule/server/UruleServerApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.nie.urule.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UruleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UruleServerApplication.class, args);
+	}
+
+}
