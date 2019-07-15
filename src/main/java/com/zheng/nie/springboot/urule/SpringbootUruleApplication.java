521f8ee60e29e2327fe69690ddd4be7c5a37f728
niezheng1 <niezheng1@jd.com>
2019-07-15 10:01:59 +0800

    init

---
+++ src/main/java/com/zheng/nie/springboot/urule/SpringbootUruleApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.nie.springboot.urule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUruleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUruleApplication.class, args);
+	}
+
+}
